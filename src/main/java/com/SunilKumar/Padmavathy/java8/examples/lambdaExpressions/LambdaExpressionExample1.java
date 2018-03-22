fb655b739e8815ded75ae3609a29d8b14061d999
Sunil Kumar Padmavathy <SunilKumar.Padmavathy@gmail.com>
2018-03-21 22:47:10 -0400

    created 2 examples for java lambda expressions

---
+++ src/main/java/com/SunilKumar/Padmavathy/java8/examples/lambdaExpressions/LambdaExpressionExample1.java
@@ -0,0 +1,33 @@
+package com.SunilKumar.Padmavathy.java8.examples.lambdaExpressions;
+
+import java.util.Arrays;
+
+import com.SunilKumar.Padmavathy.java8.examples.anonymousClasses.Car;
+
+public class LambdaExpressionExample1 {
+
+	public static void main(String... args) {
+
+		/**
+		 * Here we are reusing the implementation from AnonymousClassesExample. Instead
+		 * of Car honda = new Car { }; we are just specifying Car honda = () ->{}
+		 * 
+		 * Car honda = <arguments> <arrow token> <implementation>
+		 */
+		Car honda = () -> {
+			// Here, we don't have any arguments to pass, so it is empty ()
+			// Below is the method body
+
+			String[] additionalFeatures = { "Honda Sensing", "One touch power moon roof", "8 way power driver's seat",
+					"Multi-Angle Rearview Lamp" };
+
+			String features = Arrays.toString(additionalFeatures);
+			return features;
+
+		};
+
+		System.out.println(honda.getAdditionalFeatures());
+		System.out.println(honda.getBasicFeatures());
+
+	}
+}
---
+++ src/main/java/com/SunilKumar/Padmavathy/java8/examples/lambdaExpressions/LambdaExpressionExample2.java
@@ -0,0 +1,22 @@
+package com.SunilKumar.Padmavathy.java8.examples.lambdaExpressions;
+
+public class LambdaExpressionExample2 {
+
+	public static void main(String[] args) {
+		/**
+		 * Java 8 introduces the concept of Functional Interface. An interface with one single abstract method is considered by the compiler
+		 * as a functional interface. An annotation @FunctionalInterface is used to represent such interfaces. However, the annotation is optional.
+		 * 
+		 * Interface Runnable has a single abstract method run(). This interface is a Functional Interface
+		 * 
+		 * Below is an example of how we can use java lambdas to implement methods of functional interface.
+		 * Lambda provides easy way to create anonymous classes during runtime.
+		 * 
+		 */
+
+		Runnable runnable = () -> System.out.println("implementation of run() method through lambda");
+		runnable.run();
+		
+	}
+
+}
